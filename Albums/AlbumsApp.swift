307035bc77fbd44f6b824dbdd67ca33def8cd7d0
Florian Bruder <florian.bruder@posteo.de>
2021-12-23 18:23:39 +0100

    Initial Commit

---
+++ Albums/AlbumsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlbumsApp.swift
+//  Albums
+//
+//  Created by Florian Bruder on 23.12.21.
+//
+
+import SwiftUI
+
+@main
+struct AlbumsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

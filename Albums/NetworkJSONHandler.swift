1a39a8710fc33da56f6674f58a4d66b5724422f0
Florian Bruder <florian.bruder@posteo.de>
2021-12-24 14:46:20 +0100

    Add json function + test

--- Albums/NetworkJSONHandler.swift
+++ Albums/NetworkJSONHandler.swift
@@ -5,4 +5,32 @@
 //  Created by Florian Bruder on 24.12.21.
 //
 
+// swiftlint:disable nesting
+
 import Foundation
+
+struct NetworkJSONHandler {
+    struct Error: Swift.Error {
+        enum Code {
+            case mimeTypeError
+        }
+
+        let code: Self.Code
+        let underlying: Swift.Error?
+
+        init(
+            _ code: Self.Code,
+            underlying: Swift.Error? = nil
+        ) {
+            self.code = code
+            self.underlying = underlying
+        }
+    }
+
+    static func json(
+        with: Data,
+        response: URLResponse
+    ) throws {
+        throw Self.Error(.mimeTypeError)
+    }
+}
--- AlbumsTests/NetworkJSONHandlerTests.swift
+++ AlbumsTests/NetworkJSONHandlerTests.swift
@@ -5,4 +5,25 @@
 //  Created by Florian Bruder on 24.12.21.
 //
 
-import Foundation
+@testable import Albums
+import XCTest
+
+final class NetworkJSONHandlerTestCase: XCTestCase {}
+
+extension NetworkDataHandlerTestCase {
+    func testMimeTypeError() {
+        let response = HTTPURLResponseTestDouble(headerFields: ["CONTENT-TYPE": "IMAGE/PNG"])
+
+        XCTAssertThrowsError(
+            try NetworkJSONHandler.json(
+                with: DataTestDouble(),
+                response: response
+            )
+        ) { error in
+            if let error = try? XCTUnwrap(error as? NetworkJSONHandler.Error) {
+                XCTAssertEqual(error.code, .mimeTypeError)
+                XCTAssertNil(error.underlying)
+            }
+        }
+    }
+}

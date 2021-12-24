da99d402f04bba56af6a1489076a6f49f95d6663
Florian Bruder <florian.bruder@posteo.de>
2021-12-24 10:29:50 +0100

    Add test helper functions

--- AlbumsTests/AlbumsTests.swift
+++ AlbumsTests/AlbumsTests.swift
@@ -5,32 +5,46 @@
 //  Created by Florian Bruder on 23.12.21.
 //
 
-import XCTest
 @testable import Albums
+import XCTest
 
-class AlbumsTests: XCTestCase {
+final class AlbumsTests: XCTestCase {}
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+// swiftlint:disable identifier_name
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+func DataTestDouble() -> Data {
+    Data(UInt8.min ... UInt8.max)
+}
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
+func HTTPURLResponseTestDouble(
+    statusCode: Int = 200,
+    headerFields: [String: String]? = nil
+) -> HTTPURLResponse {
+    HTTPURLResponse(
+        url: URLTestDouble(),
+        statusCode: statusCode,
+        httpVersion: "HTTP/1.1",
+        headerFields: headerFields
+    )!
+}
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+func NSErrorTestDouble() -> NSError {
+    NSError(domain: "", code: 0)
+}
 
+func URLRequestTestData() -> URLRequest {
+    URLRequest(url: URLTestDouble())
+}
+
+func URLResponseTestDouble() -> URLResponse {
+    URLResponse(
+        url: URLTestDouble(),
+        mimeType: nil,
+        expectedContentLength: 0,
+        textEncodingName: nil
+    )
+}
+
+func URLTestDouble() -> URL {
+    URL(string: "http://localhost/")!
 }

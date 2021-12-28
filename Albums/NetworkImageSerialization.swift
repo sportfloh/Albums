0d58beb4dbe8ba3bf3ecb1bafd73bcf572469673
Florian Bruder <florian.bruder@posteo.de>
2021-12-28 22:40:10 +0100

    Add first implementation of NetworkImageSerialization + testImageSourceError

--- Albums/NetworkImageSerialization.swift
+++ Albums/NetworkImageSerialization.swift
@@ -5,7 +5,7 @@
 //  Created by Florian Bruder on 28.12.21.
 //
 
-// swiftlint:disable identifier_name
+// swiftlint:disable identifier_name nesting
 
 import Foundation
 
@@ -18,3 +18,30 @@ protocol NetworkImageSerializationImageSource {
 }
 
 extension NetworkImageSource: NetworkImageSerializationImageSource {}
+
+struct NetworkImageSerialization<ImageSource: NetworkImageSerializationImageSource> {}
+
+extension NetworkImageSerialization {
+    struct Error: Swift.Error {
+        enum Code {
+            case imageSourceError
+        }
+
+        let code: Self.Code
+        let underlying: Swift.Error?
+
+        init(_ code: Self.Code, underlying: Swift.Error? = nil) {
+            self.code = code
+            self.underlying = underlying
+        }
+    }
+}
+
+extension NetworkImageSerialization {
+    static func image(with data: Data) throws -> ImageSource.Image {
+        let imageSource = ImageSource.createImageSource(
+            with: data as CFData,
+            options: nil)
+        throw Self.Error(.imageSourceError)
+    }
+}
--- AlbumsTests/NetworkImageSerializationTests.swift
+++ AlbumsTests/NetworkImageSerializationTests.swift
@@ -5,6 +5,80 @@
 //  Created by Florian Bruder on 28.12.21.
 //
 
+@testable import Albums
 import XCTest
 
-final class NetworkImageSerializationTestCase: XCTestCase {}
+final class NetworkImageSerializationTestCase: XCTestCase {
+    private typealias NetworkImageSerializationTestDouble = NetworkImageSerialization<ImageSourceTestDouble>
+
+    override func tearDown() {
+        ImageSourceTestDouble.imageSourceParameterData = nil
+        ImageSourceTestDouble.imageSourceParameterOptions = nil
+        ImageSourceTestDouble.imageSourceReturnImageSource = nil
+
+        ImageSourceTestDouble.imageParameterImageSource = nil
+        ImageSourceTestDouble.imageParameterIndex = nil
+        ImageSourceTestDouble.imageParameterOptions = nil
+        ImageSourceTestDouble.imageReturnImage = nil
+    }
+}
+
+extension NetworkImageSerializationTestCase {
+    private struct ImageSourceTestDouble: NetworkImageSerializationImageSource {
+        static var imageSourceParameterData: CFData?
+        static var imageSourceParameterOptions: CFDictionary?
+        static var imageSourceReturnImageSource: NSObject?
+
+        static func createImageSource(
+            with data: CFData,
+            options: CFDictionary?
+        ) -> NSObject? {
+            self.imageSourceParameterData = data
+            self.imageSourceParameterOptions = options
+            return self.imageSourceReturnImageSource
+        }
+
+        static var imageParameterImageSource: NSObject?
+        static var imageParameterIndex: Int?
+        static var imageParameterOptions: CFDictionary?
+        static var imageReturnImage: NSObject?
+
+        static func createImage(
+            with imageSource: NSObject,
+            at index: Int,
+            options: CFDictionary?
+        ) -> NSObject? {
+            self.imageParameterImageSource = imageSource
+            self.imageParameterIndex = index
+            self.imageParameterOptions = options
+            return self.imageReturnImage
+        }
+    }
+}
+
+extension NetworkImageSerializationTestCase {
+    func testImageSourceError() {
+        ImageSourceTestDouble.imageSourceReturnImageSource = nil
+
+        ImageSourceTestDouble.imageReturnImage = nil
+
+        XCTAssertThrowsError(
+            try NetworkImageSerializationTestDouble.image(with: DataTestDouble())
+        ) { error in
+            XCTAssertEqual(
+                ImageSourceTestDouble.imageSourceParameterData as Data?,
+                DataTestDouble()
+            )
+            XCTAssertNil(ImageSourceTestDouble.imageSourceParameterOptions)
+
+            XCTAssertNil(ImageSourceTestDouble.imageParameterImageSource)
+            XCTAssertNil(ImageSourceTestDouble.imageParameterIndex)
+            XCTAssertNil(ImageSourceTestDouble.imageParameterOptions)
+
+            if let error = try? XCTUnwrap(error as? NetworkImageSerializationTestDouble.Error) {
+                XCTAssertEqual(error.code, .imageSourceError)
+                XCTAssertNil(error.underlying)
+            }
+        }
+    }
+}

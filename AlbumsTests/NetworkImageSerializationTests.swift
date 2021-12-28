a258ace695120c7abc136dc8e1f4ff742834d5ef
Florian Bruder <florian.bruder@posteo.de>
2021-12-28 21:05:07 +0100

    Add NetworkImageSerializationTests.swift file

---
+++ AlbumsTests/NetworkImageSerializationTests.swift
@@ -0,0 +1,10 @@
+//
+//  NetworkImageSerializationTests.swift
+//  AlbumsTests
+//
+//  Created by Florian Bruder on 28.12.21.
+//
+
+import XCTest
+
+final class NetworkImageSerializationTestCase: XCTestCase {}

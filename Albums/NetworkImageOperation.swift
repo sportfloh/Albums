26fc2369e7d7bfe0b8c80d83bcfb7b4ee8a9ebb0
Florian Bruder <florian.bruder@posteo.de>
2022-01-05 13:01:46 +0100

    Add protocols for NetworkImageOperation

--- Albums/NetworkImageOperation.swift
+++ Albums/NetworkImageOperation.swift
@@ -7,3 +7,25 @@
 
 import Foundation
 
+// MARK: -
+
+protocol NetworkImageOperationSession {
+    static func data(for: URLRequest) async throws -> (Data, URLResponse)
+}
+
+extension NetworkSession: NetworkImageOperationSession where URLSession == Foundation.URLSession {}
+
+// MARK: -
+
+protocol NetworkImageOperationImageHandler {
+    associatedtype Image
+
+    static func image(with: Data, response: URLResponse) throws -> Image
+}
+
+extension NetworkImageHandler: NetworkImageOperationImageHandler where
+    DataHandler == NetworkDataHandler,
+    ImageSerialization == NetworkImageSerialization<NetworkImageSource>
+{}
+
+// MARK: -

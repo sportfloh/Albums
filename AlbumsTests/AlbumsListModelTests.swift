1ddeebeb2e67cb74ac25d35832d29b678752a033
Florian Bruder <florian.bruder@posteo.de>
2022-01-06 11:23:49 +0100

    Add test double for AlbumsListModelTests

--- AlbumsTests/AlbumsListModelTests.swift
+++ AlbumsTests/AlbumsListModelTests.swift
@@ -5,4 +5,34 @@
 //  Created by Florian Bruder on 06.01.22.
 //
 
-import Foundation
+@testable import Albums
+import XCTest
+
+// MARK: -
+
+final class AlbumsListModelTestCase: XCTest {
+    override func tearDown() {
+        JSONOperationTestDouble.parameterRequest = nil
+        JSONOperationTestDouble.returnJSON = nil
+    }
+}
+
+// MARK: - Test Doubles
+
+extension AlbumsListModelTestCase {
+    private typealias AlbumsListModelTestDouble = AlbumsListModel<JSONOperationTestDouble>
+
+    private struct JSONOperationTestDouble: AlbumsListModelJSONOperation {
+        static var parameterRequest: URLRequest?
+        static var returnJSON: Any?
+        static let returnError = NSErrorTestDouble()
+
+        static func json(for request: URLRequest) async throws -> Any {
+            self.parameterRequest = request
+            guard let returnJSON = self.returnJSON else {
+                throw self.returnError
+            }
+            return returnJSON
+        }
+    }
+}

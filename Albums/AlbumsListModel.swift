c1f7d8a7ef5f2c12a82502177a0a1268d5d3b9b6
Florian Bruder <florian.bruder@posteo.de>
2022-01-06 15:10:41 +0100

    Add AlbumsListModel + tests

--- Albums/AlbumsListModel.swift
+++ Albums/AlbumsListModel.swift
@@ -5,7 +5,7 @@
 //  Created by Florian Bruder on 06.01.22.
 //
 
-// swiftlint:disable line_length
+// swiftlint:disable line_length identifier_name
 
 import Foundation
 
@@ -33,6 +33,30 @@ extension Album: Identifiable {}
 
 // MARK: -
 
+private func Albums(_ json: Any) -> [Album] {
+    var albums = [Album]()
+    if let array = ((json as? [String: Any])?["feed"] as? [String: Any])?["entry"] as? [[String: Any]] {
+        for dictionary in array {
+            if let artist = ((dictionary["im:artist"] as? [String: Any])?["label"] as? String),
+               let name = ((dictionary["im:name"] as? [String: Any])?["label"] as? String),
+               let image = ((dictionary["im:image"] as? [[String: Any]])?[2]["label"] as? String),
+               let id = (((dictionary["id"] as? [String: Any])?["attributes"] as? [String: Any])?["im:id"] as? String)
+            {
+                let album = Album(
+                    id: id,
+                    artist: artist,
+                    name: name,
+                    image: image
+                )
+                albums.append(album)
+            }
+        }
+    }
+    return albums
+}
+
+// MARK: -
+
 @MainActor final class AlbumsListModel<JSONOperation: AlbumsListModelJSONOperation>: ObservableObject {
     @Published private(set) var albums = [Album]()
 }
@@ -41,7 +65,8 @@ extension AlbumsListModel {
     func requestAlbums() async throws {
         if let url = URL(string: "https://itunes.apple.com/us/rss/topalbums/limit=100/json") {
             let request = URLRequest(url: url)
-            try await JSONOperation.json(for: request)
+            let json = try await JSONOperation.json(for: request)
+            albums = Albums(json)
         }
     }
 }
--- AlbumsTests/AlbumsListModelTests.swift
+++ AlbumsTests/AlbumsListModelTests.swift
@@ -5,6 +5,8 @@
 //  Created by Florian Bruder on 06.01.22.
 //
 
+// swiftlint:disable identifier_name force_try
+
 @testable import Albums
 import XCTest
 
@@ -60,5 +62,81 @@ extension AlbumsListModelTestCase {
                 XCTAssertIdentical(error, JSONOperationTestDouble.returnError)
             }
         }
+    }
+}
+
+// MARK: -
+
+private func Albums(_ json: Any) -> [Album] {
+    var albums = [Album]()
+    if let array = ((json as? [String: Any])?["feed"] as? [String: Any])?["entry"] as? [[String: Any]] {
+        for dictionary in array {
+            if let artist = ((dictionary["im:artist"] as? [String: Any])?["label"] as? String),
+               let name = ((dictionary["im:name"] as? [String: Any])?["label"] as? String),
+               let image = ((dictionary["im:image"] as? [[String: Any]])?[2]["label"] as? String),
+               let id = (((dictionary["id"] as? [String: Any])?["attributes"] as? [String: Any])?["im:id"] as? String)
+            {
+                let album = Album(
+                    id: id,
+                    artist: artist,
+                    name: name,
+                    image: image
+                )
+                albums.append(album)
+            }
+        }
+    }
+    return albums
+}
+
+// MARK: -
+
+extension AlbumsListModelTestCase {
+    private static var json: Any {
+        let bundle = Bundle(identifier: "de.sportfloh.AlbumsTests")!
+        let url = bundle.url(forResource: "Albums", withExtension: "json")!
+        let data = try! Data(contentsOf: url)
+        let json = try! JSONSerialization.jsonObject(with: data, options: [])
+        return json
+    }
+
+    private static var albums: [Album] {
+        Albums(self.json)
+    }
+
+    @MainActor func testSuccess() async {
+        JSONOperationTestDouble.returnJSON = Self.json
+
+        let model = AlbumsListModelTestDouble()
+
+        var modelDidChange = false
+        let modelWillChange = model.objectWillChange.sink { _ in
+            modelDidChange = true
+        }
+
+        var albumsDidChange = false
+        let albumsWillChange = model.$albums.sink { _ in
+            if modelDidChange {
+                albumsDidChange = true
+            }
+        }
+
+        do {
+            try await model.requestAlbums()
+
+            XCTAssertTrue(albumsDidChange)
+
+            XCTAssertEqual(
+                JSONOperationTestDouble.parameterRequest,
+                Self.request
+            )
+
+            XCTAssertEqual(model.albums, Self.albums)
+        } catch {
+            XCTFail("testSuccess failed")
+        }
+
+        modelWillChange.cancel()
+        albumsWillChange.cancel()
     }
 }

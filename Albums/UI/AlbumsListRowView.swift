ad903bba200e1afa713aea97d9acba7aa531970b
Florian Bruder <florian.bruder@posteo.de>
2022-01-08 22:25:46 +0100

    Add AlbumsListRowModel + preview

--- Albums/UI/AlbumsListRowView.swift
+++ Albums/UI/AlbumsListRowView.swift
@@ -5,16 +5,128 @@
 //  Created by Florian Bruder on 08.01.22.
 //
 
+// swiftlint:disable line_length
+
 import SwiftUI
 
-struct AlbumsListRowView: View {
+// MARK: -
+
+@MainActor protocol AlbumsListRowViewModel: ObservableObject {
+    var artist: String { get }
+    var name: String { get }
+    var image: CGImage? { get }
+
+    init(album: Album)
+
+    func requestImage() async throws
+}
+
+extension AlbumsListRowModel: AlbumsListRowViewModel where ImageOperation == NetworkImageOperation<NetworkSession<URLSession>, NetworkImageHandler<NetworkDataHandler, NetworkImageSerialization<NetworkImageSource>>> {}
+
+// MARK: -
+
+struct AlbumsListRowView<ListRowViewModel: AlbumsListRowViewModel>: View {
+    @ObservedObject private var model: ListRowViewModel
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            if let cgImage = self.model.image {
+                Image(decorative: cgImage, scale: 0, orientation: .up)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 128, height: 128, alignment: .topLeading)
+            }
+            VStack(alignment: .leading, spacing: 3) {
+                Text(self.model.artist)
+                    .foregroundColor(.primary)
+                    .font(.headline)
+                Text(self.model.name)
+                    .foregroundColor(.secondary)
+                    .font(.subheadline)
+            }
+        }.task {
+            do {
+                try await self.model.requestImage()
+            } catch {
+                print(error)
+            }
+        }
+    }
+
+    init(model: ListRowViewModel) {
+        self.model = model
     }
 }
 
+// MARK: -
+
 struct AlbumsListRowView_Previews: PreviewProvider {
+    private final class ListRowModel: AlbumsListRowViewModel {
+        let artist: String
+        let name: String
+
+        @Published private(set) var image: CGImage?
+
+        init(album: Album) {
+            self.artist = album.artist
+            self.name = album.name
+        }
+
+        func requestImage() async throws {
+            if let context = CGContext(
+                data: nil,
+                width: 256,
+                height: 256,
+                bitsPerComponent: 8,
+                bytesPerRow: 0,
+                space: CGColorSpaceCreateDeviceRGB(),
+                bitmapInfo: CGImageAlphaInfo.premultipliedLast.rawValue
+            ) {
+                context.setFillColor(
+                    red: 0.5,
+                    green: 0.5,
+                    blue: 0.5,
+                    alpha: 1
+                )
+                context.fill(
+                    CGRect(
+                        x: 0,
+                        y: 0,
+                        width: 256,
+                        height: 256
+                    )
+                )
+                self.image = context.makeImage()
+            }
+        }
+    }
+
+    static var albums: [Album] {
+        [
+            Album(
+                id: "Rubber Soul",
+                artist: "Beatles",
+                name: "Rubber Soul",
+                image: "http://localhost/rubber-soul.jpeg"
+            ),
+            Album(
+                id: "Pet Sounds",
+                artist: "Beach Boys",
+                name: "Pet Sounds",
+                image: "http://localhost/pet-sounds.jpeg"
+            )
+        ]
+    }
+
     static var previews: some View {
-        AlbumsListRowView()
+        List(
+            self.albums
+        ) { album in
+            AlbumsListRowView<ListRowModel>(
+                model: ListRowModel(album: album)
+            )
+        }.listStyle(
+            .plain
+        )
     }
 }

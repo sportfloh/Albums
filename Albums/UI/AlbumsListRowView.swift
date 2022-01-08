e22a22c31971d6b6d29bf761d739fc12a44bdc46
Florian Bruder <florian.bruder@posteo.de>
2022-01-08 21:17:45 +0100

    Add AlbumsListRowView.swift

---
+++ Albums/UI/AlbumsListRowView.swift
@@ -0,0 +1,20 @@
+//
+//  AlbumsListRowView.swift
+//  Albums
+//
+//  Created by Florian Bruder on 08.01.22.
+//
+
+import SwiftUI
+
+struct AlbumsListRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AlbumsListRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        AlbumsListRowView()
+    }
+}

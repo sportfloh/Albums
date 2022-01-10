0f779beb6146bc3bd93e79aa28d8dea59c9d6251
Florian Bruder <florian.bruder@posteo.de>
2022-01-10 12:05:48 +0100

    Add protocol AlbumsListViewModel + conformance

--- Albums/UI/AlbumsListView.swift
+++ Albums/UI/AlbumsListView.swift
@@ -9,6 +9,16 @@ import SwiftUI
 
 // MARK: -
 
+@MainActor protocol AlbumsListViewModel: ObservableObject {
+    var albums: [Album] { get }
+
+    func requestAlbums() async throws
+}
+
+extension AlbumsListModel: AlbumsListViewModel where JSONOperation == NetworkJSONOperation<NetworkSession<Foundation.URLSession>, NetworkJSONHandler<NetworkDataHandler, Foundation.JSONSerialization>> {}
+
+// MARK: -
+
 struct AlbumsListView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)

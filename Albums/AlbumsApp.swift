3fc9346fa54eef9e27cfcdd4b67dad19ef16f410
Florian Bruder <florian.bruder@posteo.de>
2022-01-10 12:40:57 +0100

    Add AlbumsListView to AlbumsApp

--- Albums/AlbumsApp.swift
+++ Albums/AlbumsApp.swift
@@ -5,13 +5,30 @@
 //  Created by Florian Bruder on 23.12.21.
 //
 
+// swiftlint:disable line_length
+
 import SwiftUI
 
 @main
 struct AlbumsApp: App {
+    private typealias JSONHandler = NetworkJSONHandler<NetworkDataHandler, JSONSerialization>
+    private typealias ImageHandler = NetworkImageHandler<NetworkDataHandler, NetworkImageSerialization<NetworkImageSource>>
+
+    private typealias JSONOperation = NetworkJSONOperation<NetworkSession<URLSession>, JSONHandler>
+    private typealias ImageOperation = NetworkImageOperation<NetworkSession<URLSession>, ImageHandler>
+
+    private typealias ListModel = AlbumsListModel<JSONOperation>
+    private typealias ListRowModel = AlbumsListRowModel<ImageOperation>
+
+    private typealias ListView = AlbumsListView<ListModel, ListRowModel>
+
+    @StateObject private var model = ListModel()
+
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ListView(
+                model: self.model
+            )
         }
     }
 }
